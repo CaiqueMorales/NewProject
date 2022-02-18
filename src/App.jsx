83d32c2287c7cfbc35b63884c4da4604c8e437d8
Caique <aecaique@gmail.com>
2022-02-18 12:08:46 -0300

    navbar style done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import { Home } from "./components/Home";
+
+import './App.css';
+
 export function App () {
 
     return(
 
-        <>
+        <Router>
         
-            App
+            <NavBar />
+            <Routes>
+
+                <Route exact path="/" element={<Home />}>
+                </Route>
+
+            </Routes>
         
-        </>
+        </Router>
 
     )
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './NavBar.css';
+
+export function NavBar () {
+
+    const [click, setClick] = useState(false)
+
+    const handleClick = (() => setClick(!click))
+
+    const setClickFalse = (() => setClick(false))
+
+    return(
+
+        <>
+        
+            <header id="header" className="header">
+                <nav className="nav container">
+                    <Link to="/">Logo</Link>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li onClick={setClickFalse} className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+                        <li onClick={setClickFalse} className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+                        <li onClick={setClickFalse} className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+                        <li onClick={setClickFalse} className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+                        <li onClick={setClickFalse} className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+                    </ul>
+                    <div onClick={handleClick} className="hamburger-button">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                </nav>
+            </header>
+        
+        </>
+
+    )
+
+}
